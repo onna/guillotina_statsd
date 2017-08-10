b7f5332c2a9bfed80aad20b1caa4ffe7237641c0
vangheem <vangheem@gmail.com>
2017-08-10 16:05:18 -0500

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='guillotina_statsd',
-    version="1.0.0",
+    version='1.0.1.dev0',
     description='Integrate statsd into guillotina',
     long_description=README,
     install_requires=[
